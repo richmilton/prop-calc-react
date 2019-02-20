c4b7385a13093dc03f65f8a6fe9a6646096d35e2
richard <richardmilton@ahotclub.com>
2019-02-20 15:16:41 +0000

    clean up

--- src/components/formconfig.js
+++ src/components/formconfig.js
@@ -50,7 +50,10 @@ const fields = [
     label: 'Stamp duty',
     name: 'stampDutyType',
     type: 'select',
-    options: [{value: 'residential', name: 'Residential'},{value: 'commercial', name: 'Commercial'}],
+    options: [
+      {value: 'residential', name: 'Residential'},
+      {value: 'commercial', name: 'Commercial'}
+      ],
     defVal: 'residential'
   },
   {
@@ -87,14 +90,12 @@ const fields = [
     label: 'Letting agents rate(%)',
     name: 'agentsPercent',
     type: 'number',
-    options: [[0,0],[6,6],[7,7],[8,8],[9,9],[10,10],[11,11]],
     defVal: 10
   },
   {
     label: 'MOE rate(%)',
     name: 'moePercent',
     type: 'number',
-    options: [[0,0],[10,10],[12,12],[14,14],[15,15],[16,16],[18,18]],
     defVal: 15
   },
   {
@@ -107,14 +108,12 @@ const fields = [
     label: 'Interest rate(%)',
     name: 'mortgageInterestRatePercent',
     type: 'number',
-    options: [[1,'1.0'],[1.5,'1.5'],[2,'2.0'],[2.5,'2.5'],[3,'3.0'],[3.5,'3.5'],[4,'4.0'],[4.5,'4.5'],[5,'5.0'],[5.5,'5.5'],[6,'6.0'],[6.5,'6.5'],[7,'7.0'],[7.5,'7.5'],[8,'8.0'],[8.5,'8.5'],[9,'9.0'],[9.5,'9.5'],[10,'10.0']],
     defVal: 6.0
   },
   {
     label: 'Loan to Value(%)',
     name: 'loanToValue',
     type: 'number',
-    options: [[50,50],[55,55],[60,60],[65,65],[70,70],[75,75],[80,80]],
     defVal: 75
   },
 
@@ -134,7 +133,6 @@ const fields = [
     label: 'Mortgage stress test(%)',
     name: 'mortgageStressMultipePercent',
     type: 'number',
-    options: [[125,125],[130,130],[135,135],[140,140],[145,145],[150,150],[155,155],[160,160]],
     defVal: 125
   },
   {
