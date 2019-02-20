51425cc11c62ae469c142cfa147d0ad2c0ab8c01
richard <richardmilton@ahotclub.com>
2019-02-20 03:25:15 +0000

    full set of tests for stamp duty

--- src/test/logic/stampduty/stampduty-calculator.test.js
+++ src/test/logic/stampduty/stampduty-calculator.test.js
@@ -1,20 +1,78 @@
 import calculateStampDuty from '../../../logic/stampduty/stampduty-calculator';
-import calculations from "../../../logic/calculations";
 const {expect} = require('chai');
 
-describe('calculations.calculateStampDuty()', function () {
-  it('should return a value', function () {
+describe('calculations.calculateStampDuty(val, "residential")', function () {
+  it('should return correct values for residential', function () {
 
-    let sd = calculateStampDuty(200000, 'commercial');
-    console.log(sd);
-    expect(sd).to.be.equal(1000);
-    sd = calculateStampDuty(2000000, 'commercial');
-    expect(sd).to.be.equal(89500);
+    let sd = calculateStampDuty(100000, 'residential');
+    expect(sd).to.be.equal(3000);
 
     sd = calculateStampDuty(200000, 'residential');
-    console.log(sd);
     expect(sd).to.be.equal(7500);
+
+    sd = calculateStampDuty(300000, 'residential');
+    expect(sd).to.be.equal(14000);
+
+    sd = calculateStampDuty(400000, 'residential');
+    expect(sd).to.be.equal(22000);
+
+    sd = calculateStampDuty(500000, 'residential');
+    expect(sd).to.be.equal(30000);
+
+    sd = calculateStampDuty(600000, 'residential');
+    expect(sd).to.be.equal(38000);
+
+    sd = calculateStampDuty(700000, 'residential');
+    expect(sd).to.be.equal(46000);
+
+    sd = calculateStampDuty(800000, 'residential');
+    expect(sd).to.be.equal(54000);
+
+    sd = calculateStampDuty(900000, 'residential');
+    expect(sd).to.be.equal(62000);
+
+    sd = calculateStampDuty(1000000, 'residential');
+    expect(sd).to.be.equal(73750);
+
     sd = calculateStampDuty(2000000, 'residential');
     expect(sd).to.be.equal(213750);
+  });
+});
+
+describe('calculations.calculateStampDuty()', function () {
+  it('should return correct values for commercial', function () {
+
+    let sd = calculateStampDuty(100000, 'commercial');
+    expect(sd).to.be.equal(0);
+
+    sd = calculateStampDuty(200000, 'commercial');
+    expect(sd).to.be.equal(1000);
+
+    sd = calculateStampDuty(300000, 'commercial');
+    expect(sd).to.be.equal(4500);
+
+    sd = calculateStampDuty(400000, 'commercial');
+    expect(sd).to.be.equal(9500);
+
+    sd = calculateStampDuty(500000, 'commercial');
+    expect(sd).to.be.equal(14500);
+
+    sd = calculateStampDuty(600000, 'commercial');
+    expect(sd).to.be.equal(19500);
+
+    sd = calculateStampDuty(700000, 'commercial');
+    expect(sd).to.be.equal(24500);
+
+    sd = calculateStampDuty(800000, 'commercial');
+    expect(sd).to.be.equal(29500);
+
+    sd = calculateStampDuty(900000, 'commercial');
+    expect(sd).to.be.equal(34500);
+
+    sd = calculateStampDuty(1000000, 'commercial');
+    expect(sd).to.be.equal(39500);
+
+    sd = calculateStampDuty(2000000, 'commercial');
+    expect(sd).to.be.equal(89500);
   });
 });
