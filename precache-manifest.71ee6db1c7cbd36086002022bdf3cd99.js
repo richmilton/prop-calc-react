c5b39e3f41155d76886e00b98c169ba8a3eed0d0
richard <richardmilton@ahotclub.com>
2019-02-22 10:26:38 +0000

    Updates

--- precache-manifest.71ee6db1c7cbd36086002022bdf3cd99.js
+++ precache-manifest.71ee6db1c7cbd36086002022bdf3cd99.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/prop-calc-react/static/js/runtime~main.82c623cd.js"
   },
   {
-    "revision": "2c4b75858169b89df4bc",
-    "url": "/prop-calc-react/static/js/main.2c4b7585.chunk.js"
+    "revision": "cb62dc5e12bb3b71b7df",
+    "url": "/prop-calc-react/static/js/main.cb62dc5e.chunk.js"
   },
   {
     "revision": "72b6d25545a2f8cbcce3",
     "url": "/prop-calc-react/static/js/2.72b6d255.chunk.js"
   },
   {
-    "revision": "2c4b75858169b89df4bc",
+    "revision": "cb62dc5e12bb3b71b7df",
     "url": "/prop-calc-react/static/css/main.162e9b33.chunk.css"
   },
   {
-    "revision": "e192deacd7f6eb76e4b34e3304f40fd0",
+    "revision": "fe09f387bec9a8da1850c5827cf447cc",
     "url": "/prop-calc-react/index.html"
   }
 ];
