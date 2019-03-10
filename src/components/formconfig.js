36628aee3c2cc6081931f969ae055913cdd588c6
richard <richardmilton@ahotclub.com>
2019-03-10 02:04:09 +0000

    lint cleanup

--- src/components/formconfig.js
+++ src/components/formconfig.js
@@ -1,4 +1,3 @@
-
 const currencyPlaceholder = '##CURR##';
 const percentSign = String.fromCharCode(37);
 
@@ -12,154 +11,153 @@ const fields = [
     label: `Purchase price(${currencyPlaceholder})`,
     name: 'propertyValue',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Done up value(${currencyPlaceholder})`,
     name: 'doneUpValue',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Monthly rent(${currencyPlaceholder})`,
     name: 'monthlyRent',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Initial LTV(${percentSign})`,
     name: 'initialLoanToValue',
     type: 'number',
-    defVal: 75
+    defVal: 75,
   },
   {
     label: 'Cash purchase',
     name: 'buyingCash',
     type: 'checkbox',
-    defVal: 'no'
+    defVal: 'no',
   },
   {
     label: `Surveyors fee(${currencyPlaceholder})`,
     name: 'initSurveyorsFee',
     type: 'number',
-    defVal: 600
+    defVal: 600,
   },
   {
     label: `Mortgage fee(${currencyPlaceholder})`,
     name: 'initMortgageFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Legal fees(${currencyPlaceholder})`,
     name: 'initLegalFee',
     type: 'number',
-    defVal: 1500
+    defVal: 1500,
   },
   {
     label: 'Stamp duty',
     name: 'stampDutyType',
     type: 'select',
     options: [
-      {value: 'residential', name: 'Residential Eng/NI'},
-      {value: 'commercial', name: 'Commercial Eng/NI'},
-      {value: 'residentialWales', name: 'Residential Wales'},
-      {value: 'commercialWales', name: 'Commercial Wales'},
-      {value: 'residentialScotland', name: 'Residential Scotland'},
-      {value: 'commercialScotland', name: 'Commercial Scotland'},
-      {value: 'ireland', name: 'Ireland'}
-      ],
-    defVal: 'residential'
+      { value: 'residential', name: 'Residential Eng/NI' },
+      { value: 'commercial', name: 'Commercial Eng/NI' },
+      { value: 'residentialWales', name: 'Residential Wales' },
+      { value: 'commercialWales', name: 'Commercial Wales' },
+      { value: 'residentialScotland', name: 'Residential Scotland' },
+      { value: 'commercialScotland', name: 'Commercial Scotland' },
+      { value: 'ireland', name: 'Ireland' },
+    ],
+    defVal: 'residential',
   },
   {
     label: `Refurbishment cost(${currencyPlaceholder})`,
     name: 'refurbCost',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Other cost(${currencyPlaceholder})`,
     name: 'otherCost',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Remortgage fee(${currencyPlaceholder})`,
     name: 'remortgageFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Remortgage valuation fee(${currencyPlaceholder})`,
     name: 'remortgageValuationFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Remortgage legal fees(${currencyPlaceholder})`,
     name: 'remortgageLegalFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Letting agents rate(${percentSign})`,
     name: 'agentsPercent',
     type: 'number',
-    defVal: 10
+    defVal: 10,
   },
   {
     label: `
     MOE rate(${percentSign})`,
     name: 'moePercent',
     type: 'number',
-    defVal: 15
+    defVal: 15,
   },
   {
     label: 'Repairing lease',
     name: 'repairingLease',
     type: 'checkbox',
-    defVal: 'no'
+    defVal: 'no',
   },
   {
     label: `Interest rate(${percentSign})`,
     name: 'mortgageInterestRatePercent',
     type: 'number',
-    defVal: 6.0
+    defVal: 6.0,
   },
   {
     label: `Remortgage LTV(${percentSign})`,
     name: 'loanToValue',
     type: 'number',
-    defVal: 75
+    defVal: 75,
   },
   {
     label: `Solicitors selling fee(${currencyPlaceholder})`,
     name: 'sellingLegalFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Agent/selling fee(${currencyPlaceholder})`,
     name: 'agentSellingFee',
     type: 'number',
-    defVal: 0
+    defVal: 0,
   },
   {
     label: `Mortgage stress test(${percentSign})`,
     name: 'mortgageStressMultipePercent',
     type: 'number',
-    defVal: 125
+    defVal: 125,
   },
   {
     label: `Stress interest rate(${percentSign})`,
     name: 'mortgageStressInterestRatePercent',
     type: 'number',
-    defVal: 5.0
-  }
-
+    defVal: 5.0,
+  },
 ];
 
 module.exports = {
   fields,
-  currencyPlaceholder
+  currencyPlaceholder,
 };
