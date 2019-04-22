4e78d289734f57bf5fcb679d58e4590c5ddb8e3c
richard <richardmilton@ahotclub.com>
2019-04-22 15:17:23 +0100

    Updates

---
+++ precache-manifest.79120142a204fdac4398b8dc51a0642e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78f0a9ad2c79a5eb3058",
+    "url": "/prop-calc-react/static/js/runtime~main.c78de9d7.js"
+  },
+  {
+    "revision": "e2cfaed741a4fad8db52",
+    "url": "/prop-calc-react/static/js/main.a39cb79a.chunk.js"
+  },
+  {
+    "revision": "c5ab8963e3b231957d10",
+    "url": "/prop-calc-react/static/js/2.f49bd0af.chunk.js"
+  },
+  {
+    "revision": "e2cfaed741a4fad8db52",
+    "url": "/prop-calc-react/static/css/main.4e6b782d.chunk.css"
+  },
+  {
+    "revision": "c5ab8963e3b231957d10",
+    "url": "/prop-calc-react/static/css/2.99376adc.chunk.css"
+  },
+  {
+    "revision": "41d2ba8c3de02dcbddc28962435fe3da",
+    "url": "/prop-calc-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/prop-calc-react/precache-manifest.94021e04da098abb508e124705f88247.js"
+  "/prop-calc-react/precache-manifest.79120142a204fdac4398b8dc51a0642e.js"
 );
 
 workbox.clientsClaim();
